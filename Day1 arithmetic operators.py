02aa36640039fa78cc0b9c0eead1e2be7e289014
Manololly <63733111+Manololly@users.noreply.github.com>
2022-07-23 23:49:25 +0530

    Add files via upload

---
+++ Day1 arithmetic operators.py
@@ -0,0 +1,33 @@
+#getting values of a and b from user
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+#Addition
+c = a + b
+print(f" The addition of a and b is {c}")
+
+#subtraction
+c = a - b
+print(f" The subtraction of a and b is {c}")
+
+#Multiplication
+c = a * b
+print(f" The multiplication of a and b is {c}")
+
+#Division
+c = a / b
+print(f" The division of a and b is {c}")
+
+#Modulus
+c = a % b
+print(f" The remainder value of a/b is {c}")
+
+#Exponentiation
+c = a ** b
+print(f" The exponent value of a and b is {c}")
+
+#Floor Division(rounds the nearest value)
+c = a // b
+print(f" The Floor division of a and b is {c}")
+
+
